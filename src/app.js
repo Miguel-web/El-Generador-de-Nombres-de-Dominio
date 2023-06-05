251ddde56c9147394155efab246a83b0cb12bb51
Miguel-web <miguelgilf@gmail.com>
2023-06-05 21:41:05 +0000

    de console lo comentado, windows onload en el puerto

--- src/app.js
+++ src/app.js
@@ -10,20 +10,45 @@ import "./assets/img/4geeks.ico";
 //   document.querySelector("#domains").innerHTML = domainGenerator();
 // };
 
+// const pronouns = ["the", "our"];
+// const adjs = ["great", "big"];
+// const nouns = ["jogger", "racoon"];
+// const extens = [".com", ".es", ".io", ".us"];
+
+// const domainGenerator = () => {
+//   for (let pronoun of pronouns) {
+//     for (let adj of adjs) {
+//       for (let noun of nouns) {
+//         for (let exten of extens) {
+//           console.log(pronoun, adj, noun, exten);
+//         }
+//       }
+//     }
+//   }
+// };
+// domainGenerator();
+
+window.onload = function() {
+  //write your code here
+  document.querySelector("#domains").innerHTML = domainGenerator();
+};
+
 const pronouns = ["the", "our"];
 const adjs = ["great", "big"];
 const nouns = ["jogger", "racoon"];
 const extens = [".com", ".es", ".io", ".us"];
 
 const domainGenerator = () => {
+  let dominio = "";
+
   for (let pronoun of pronouns) {
     for (let adj of adjs) {
       for (let noun of nouns) {
         for (let exten of extens) {
-          console.log(pronoun, adj, noun, exten);
+          dominio += `<p>${pronoun} ${adj} ${noun} ${exten}</p>`;
         }
       }
     }
   }
+  return dominio;
 };
-domainGenerator();
