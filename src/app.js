23eab7051511c51f881dcb32eee146e176f7cf97
Miguel-web <miguelgilf@gmail.com>
2023-06-05 20:22:22 +0000

    solo falta ver la iteracion for

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// const pronouns = ["the", "our"];
+// const adjs = ["great", "big"];
+// const nouns = ["jogger", "racoon"];
+// const extens = [".com", ".es", ".io", ".us"];
+
+// const domainGenerator = () => {
+//   for (let pronoun of pronouns) {
+//     for (let adj of adjs) {
+//       for (let noun of nouns) {
+//         for (let exten of extens) {
+//           console.log(pronoun, adj, noun, exten);
+//         }
+//       }
+//     }
+//   }
+// };
+// domainGenerator();
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domains").innerHTML = domainGenerator();
 };
+
+const pronouns = ["the", "our"];
+const adjs = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const extens = [".com", ".es", ".io", ".us"];
+
+const domainGenerator = () => {
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let exten of extens) {
+          return `${pronoun} ${adj} ${noun} ${exten}`;
+        }
+      }
+    }
+  }
+};
+domainGenerator();
